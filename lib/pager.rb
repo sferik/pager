67a682b764ff87fb51598e84d0b603a411138500
Erik Michaels-Ober <sferik@gmail.com>
2012-01-18 14:24:30 -0800

    Gemify code snippet by @nex3 (Nathan Weizenbaum)
    http://nex-3.com/posts/73-git-style-automatic-paging-in-ruby

--- lib/pager.rb
+++ lib/pager.rb
@@ -1,5 +1,31 @@
 require "pager/version"
 
 module Pager
-  # Your code goes here...
+
+  def page
+    return if RUBY_PLATFORM =~ /win32/
+    return unless STDOUT.tty?
+
+    read, write = IO.pipe
+
+    unless Kernel.fork # Child process
+      STDOUT.reopen(write)
+      STDERR.reopen(write) if STDERR.tty?
+      read.close
+      write.close
+      return
+    end
+
+    # Parent process, become pager
+    STDIN.reopen(read)
+    read.close
+    write.close
+
+    ENV['LESS'] = 'FSRX' # Don't page if the input is short enough
+
+    Kernel.select [STDIN] # Wait until we have input before we start the pager
+    pager = ENV['PAGER'] || 'less'
+    exec pager rescue exec "/bin/sh", "-c", pager
+  end
+
 end
